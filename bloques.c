86bafc061a2f1ee0faa0798b68b578e5805d1718
hjorge-l <haroljorgelopez@gmail.com>
2025-02-15 22:12:50 +0100

    feat(block.c): add function bmonunt

--- bloques.c
+++ bloques.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
+#include <fcntl.h>
 
-#define BLOCKSIZE 512; 
+#define BLOCKSIZE 512
+#define FALSE 0
+#define TRUE 1
+#define ERROR -1
+
+int bmount(const char *camino) {
+    int fd = open(camino, O_RDWR, 0666);
+    
+    if (fd == ERROR) {
+        perror("bmount");
+        return ERROR;
+    }
+    return fd;
+}
 
 int main(){
     printf("hola mundo soy un file sistem");
